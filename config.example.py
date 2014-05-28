cf7ffa2ce2d30a4de96957467dd00d46391599cd
patins <patins@users.noreply.github.com>
2014-05-28 16:24:37 -0400

    change default open time

--- config.example.py
+++ config.example.py
@@ -4,5 +4,5 @@ RFID_READER_SERIAL = "/dev/ttyUSB0" # use /dev/serial/by-id/...
 RELAY_GPIO_PIN = 4
 TAG_FILE = "authorized_tags.txt"
 OPEN_THRESHOLD = datetime.timedelta(seconds=5)
-OPEN_TIME = 0.5
+OPEN_TIME = 4
 LOG_FILE = "door.log"
